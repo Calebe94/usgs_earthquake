e9ced03468b91bc20031150a2fb4d841c3a53b9e
Calebe94 <calebe94@disroot.org>
2024-10-20 19:17:37 -0300

    chore(.gitignore): add __pycache__ dir
    - refactor(settings.py): refactor DATABASES configuration to use dj_database_url and
    dotenv for better handling of database settings;
    - feat(requirements.txt): add dj_database_url and python-dotenv to requirements for
    - database configuration and environment variables management

--- earthquake_app/settings.py
+++ earthquake_app/settings.py
@@ -9,9 +9,12 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
+import dj_database_url
 from pathlib import Path
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -72,12 +75,8 @@ WSGI_APPLICATION = 'earthquake_app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
