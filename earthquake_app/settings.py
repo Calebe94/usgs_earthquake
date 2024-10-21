c972b1f8f0a8a5d784c994ccd378ba8f3e5994c4
Calebe94 <calebe94@disroot.org>
2024-10-21 11:51:44 -0300

    feat(views): get earthquakes is async now
    - feat(settings.py): add 'adrf' to INSTALLED_APPS to include ADRF app in the project
    - fix(settings.py): correct STATIC_URL value to '/static/' and add STATIC_ROOT for serving static files correctly
    - feat(views.py): replace direct City.objects.get with async get_object_or_404 to handle City.DoesNotExist exception asynchronously
    - feat(views.py): add error handling for missing start_date or end_date parameters in EarthquakeSearchView
    - feat(views.py): use async functions and sync_to_async for get_earthquakes and find_closest_earthquake in EarthquakeSearchView
    - feat(requirements.txt): add 'asgiref' and 'adrf' dependencies for project requirements

--- earthquake_app/settings.py
+++ earthquake_app/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'earthquakes',
-    'rest_framework'
+    'rest_framework',
+    'adrf'
 ]
 
 MIDDLEWARE = [
@@ -116,7 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- earthquakes/views.py
+++ earthquakes/views.py
@@ -1,11 +1,13 @@
+from django.shortcuts import get_object_or_404
 from .utils import get_earthquakes, find_closest_earthquake
-from rest_framework.views import APIView
+# from rest_framework.views import APIView
+from adrf.views import APIView
+from django.http import JsonResponse
 from rest_framework.response import Response
-from django.shortcuts import render
-
-from rest_framework import generics, viewsets
+from rest_framework import generics, viewsets, status
 from .models import City
 from .serializers import CitySerializer
+from asgiref.sync import sync_to_async
 
 
 class CityCreateView(generics.CreateAPIView):
@@ -19,15 +21,27 @@ class CityViewSet(viewsets.ModelViewSet):
 
 
 class EarthquakeSearchView(APIView):
-    def get(self, request, city_id):
-        city = City.objects.get(id=city_id)
+    async def get(self, request, city_id):
+        try:
+            city = await sync_to_async(get_object_or_404)(City, id=city_id)
+        except City.DoesNotExist:
+            return Response({"message": "City not found"},
+                            status=status.HTTP_404_NOT_FOUND)
+
         start_date = request.query_params.get('start_date')
         end_date = request.query_params.get('end_date')
 
-        earthquakes = get_earthquakes(start_date, end_date)
-        closest_earthquake, distance = find_closest_earthquake(
-            city, earthquakes)
+        if not start_date or not end_date:
+            return Response({"message": "Start date and end date are required"},
+                            status=status.HTTP_400_BAD_REQUEST)
 
+        earthquakes = await get_earthquakes(start_date, end_date)
+        if earthquakes is None:
+            return Response({"message": "Failed to retrieve earthquakes"},
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        closest_earthquake, distance = await sync_to_async(
+            find_closest_earthquake)(city, earthquakes)
         if closest_earthquake:
             result = {
                 "city": city.name,
@@ -39,4 +53,4 @@ class EarthquakeSearchView(APIView):
         else:
             result = {"message": "No results found"}
 
-        return Response(result)
+        return Response(result, status=status.HTTP_200_OK)
