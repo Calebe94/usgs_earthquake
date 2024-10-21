0b8ea903bc1459b125b5a282a760e03e7f015aae
Calebe94 <calebe94@disroot.org>
2024-10-21 11:44:12 -0300

    feat(utils.py): refactor functions to be async
    - feat(utils.py): refactor get_earthquakes function to use httpx library for asynchronous HTTP requests for improved performance and error handling
    - feat(utils.py): update get_earthquakes function to be asynchronous and handle exceptions for request and HTTP status errors
    - feat(utils.py): add httpx library to requirements.txt for making asynchronous HTTP requests

--- earthquakes/utils.py
+++ earthquakes/utils.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python3
-import requests
+import httpx
 from geopy.distance import geodesic
+from typing import Any, Dict, Tuple, Optional
 
 
-def get_earthquakes(start_date, end_date, min_magnitude=5.0):
-    url = f"https://earthquake.usgs.gov/fdsnws/event/1/query.geojson"
+async def get_earthquakes(start_date: str, end_date: str, min_magnitude: float = 5.0) -> Optional[Dict[str, Any]]:
+    url = "https://earthquake.usgs.gov/fdsnws/event/1/query.geojson"
     params = {
         "starttime": start_date,
         "endtime": end_date,
         "minmagnitude": min_magnitude,
         "orderby": "time"
     }
-    response = requests.get(url, params=params)
-    return response.json()
+
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url, params=params)
+            response.raise_for_status()  # Raise an error for bad responses
+            return response.json()
+    except httpx.RequestError as e:
+        print(f"Request error: {e}")
+    except httpx.HTTPStatusError as e:
+        print(f"HTTP error: {e.response.status_code} - {e.response.text}")
+    return None
 
 
-def find_closest_earthquake(city, earthquakes):
+def find_closest_earthquake(city, earthquakes: Dict[str, Any]) -> Tuple[Optional[Dict[str, Any]], float]:
     city_coords = (city.latitude, city.longitude)
     closest_event = None
     closest_distance = float('inf')
