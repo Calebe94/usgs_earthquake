5a8d23014b89b7485b53a470dcfe174d5753288f
Calebe94 <calebe94@disroot.org>
2024-10-22 20:03:35 -0300

    feat(tasks): improved task to handle cached data
    - to handle cached earthquake data retrieval and update with new earthquake data, optimizing data fetching and
    storage for better performance and accuracy
    - feat(models.py): add static method find_cached_data to CachedEarthquakeData model

--- earthquakes/models.py
+++ earthquakes/models.py
@@ -26,3 +26,11 @@ class CachedEarthquakeData(models.Model):
 
     def __str__(self):
         return f"Cached data for {self.city.name} from {self.start_date} to {self.end_date}"
+
+    @staticmethod
+    def find_cached_data(city, start_date, end_date):
+        return CachedEarthquakeData.objects.filter(
+            city=city,
+            start_date__lte=end_date,
+            end_date__gte=start_date
+        )
--- earthquakes/tasks.py
+++ earthquakes/tasks.py
@@ -2,40 +2,65 @@
 from celery import shared_task
 from django.core.cache import cache
 from .utils import get_earthquakes, find_closest_earthquake
-from .models import City
+from .models import City, CachedEarthquakeData
 import asyncio
+from datetime import datetime, timedelta
 
 
 @shared_task
 def fetch_earthquakes_task(city_id, start_date, end_date):
-    cache_key = f"earthquake_{city_id}_{start_date}_{end_date}"
-
-    cached_result = cache.get(cache_key)
-    if cached_result:
-        return cached_result
-
     try:
         city = City.objects.get(id=city_id)
     except City.DoesNotExist:
         return {"message": "City not found"}
 
-    earthquakes = asyncio.run(get_earthquakes(start_date, end_date))
-    if earthquakes is None:
-        return {"message": "Failed to retrieve earthquakes"}
+    start_date = datetime.strptime(start_date, "%Y-%m-%d").date()
+    end_date = datetime.strptime(end_date, "%Y-%m-%d").date()
 
-    closest_earthquake, distance = find_closest_earthquake(city, earthquakes)
+    cached_entries = CachedEarthquakeData.find_cached_data(
+        city, start_date, end_date)
 
-    if closest_earthquake:
-        result = {
-            "city": city.name,
-            "magnitude": closest_earthquake['properties']['mag'],
-            "place": closest_earthquake['properties']['place'],
-            "date": closest_earthquake['properties']['time'],
-            "distance_km": distance
-        }
-    else:
-        result = {"message": "No results found"}
+    missing_ranges = []
+    current_start = start_date
 
-    cache.set(cache_key, result, timeout=60*60*24)
+    cached_data = []
 
-    return result
+    if cached_entries.exists():
+        for entry in cached_entries:
+            cached_data.append(entry.data)
+            if current_start < entry.start_date:
+                missing_ranges.append(
+                    (current_start, entry.start_date - timedelta(days=1)))
+            current_start = max(
+                current_start, entry.end_date + timedelta(days=1))
+
+    if current_start <= end_date:
+        missing_ranges.append((current_start, end_date))
+
+    new_data = []
+    for missing_start, missing_end in missing_ranges:
+        earthquakes = asyncio.run(get_earthquakes(missing_start, missing_end))
+        if earthquakes is None:
+            return {"message": "Failed to retrieve earthquakes"}
+
+        closest_earthquake, distance = find_closest_earthquake(
+            city, earthquakes)
+        if closest_earthquake:
+            new_data.append({
+                "city": city.name,
+                "magnitude": closest_earthquake['properties']['mag'],
+                "place": closest_earthquake['properties']['place'],
+                "date": closest_earthquake['properties']['time'],
+                "distance_km": distance
+            })
+
+        CachedEarthquakeData.objects.create(
+            city=city,
+            start_date=missing_start,
+            end_date=missing_end,
+            data=new_data[-1]
+        )
+
+    combined_data = cached_data + new_data
+
+    return combined_data
